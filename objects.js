6be71b3435d404d8d3a2243653161c4204665706
Aniket B <aniket.b@outlook.com>
2019-02-27 06:54:11 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  coldplay: 'yellow', 
+  }
+
+playlist['Tom Walker'] = 'Just You and I'
+playlist['JP Cooper'] = 'September'
+
+function updatePlaylist(playlist, artistName, songTitle)  playlist[artistName] = songTitle
+  return playlist
+}
